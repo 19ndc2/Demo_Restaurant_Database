6954f02387e36f47ca3c9e50793db1329885d0a3
19ndc2 <19ndc2@queensu.ca>
2025-10-25 22:17:18 -0400

    Updated database restaurant and customer names

--- Backend/restaurant.sql
+++ Backend/restaurant.sql
@@ -134,61 +134,61 @@ create table payment (
 );
 
 insert into restaurant values
-    ('MaceroniHouse', 'www.MaceroniHouse.com', '1 First Ave', 'A1A1A1', 'ON'),
-    ('CornHouse', 'www.CornHouse.com', '2 First Ave', 'A1A1A1', 'ON'),
+    ('PastaPlace', 'www.PastaPlace.com', '1 First Ave', 'A1A1A1', 'ON'),
+    ('NoodleWay', 'www.NoodleWay.com', '2 First Ave', 'A1A1A1', 'ON'),
     ('GrilledCheeseHouse', 'www.GrilledCheeseHouse.com', '3 First Ave', 'A1A1A1', 'ON'),
-    ('BabyFoodHouse', 'www.BabyFoodHouse.com', '4 First Ave', 'A1A A1', 'ON');
+    ('KebabQuai', 'www.KebabQuai.com', '4 First Ave', 'A1A A1', 'ON');
 
 insert into customer values
-    ('Peter.Griffin@gmail.com', '6131111111', 'Peter', 'Griffin', '11 Second Ave', 'B2B2B2', 'ON'),
-    ('Joe.Swanson@gmail.com', '6132222222', 'Joe', 'Swanson', '12 Second Ave', 'B2B2B2', 'ON'),
-    ('Glenn.Quagmire@gmail.com', '6133333333', 'Glenn', 'Quagmire', '13 Second Ave', 'B2B2B2', 'ON'),
-    ('Adam.West@gmail.com', '6134444444', 'Adam', 'West', '14 Second Ave', 'B2B2B2', 'ON'),
-    ('Diane.Simmons@gmail.com', '6135555555', 'Diane', 'Simmons', '15 Second Ave', 'B2B2B2', 'ON'),
-    ('Tom.Tucker@gmail.com', '6136666666', 'Tom', 'Tucker', '16 Second Ave', 'B2B2B2', 'ON');
+    ('Peter.Smith@gmail.com', '6131111111', 'Peter', 'Smith', '11 Second Ave', 'B2B2B2', 'ON'),
+    ('Joe.Taylor@gmail.com', '6132222222', 'Joe', 'Taylor', '12 Second Ave', 'B2B2B2', 'ON'),
+    ('Glenn.Li@gmail.com', '6133333333', 'Glenn', 'Li', '13 Second Ave', 'B2B2B2', 'ON'),
+    ('Adam.Johnson@gmail.com', '6134444444', 'Adam', 'Johnson', '14 Second Ave', 'B2B2B2', 'ON'),
+    ('Diane.Wilson@gmail.com', '6135555555', 'Diane', 'Wilson', '15 Second Ave', 'B2B2B2', 'ON'),
+    ('Tom.Chase@gmail.com', '6136666666', 'Tom', 'Chase', '16 Second Ave', 'B2B2B2', 'ON');
 
 insert into item values
-    (10.99, 'maceroni', 'MaceroniHouse'),
-    (9.99, 'cold corn', 'CornHouse'),
-    (8.99, 'warm corn', 'CornHouse'),
-    (7.99, 'hot corn', 'CornHouse'),
+    (10.99, 'pasta', 'PastaPlace'),
+    (9.99, 'cold noodles', 'NoodleWay'),
+    (8.99, 'warm noodles', 'NoodleWay'),
+    (7.99, 'vegan noodles', 'NoodleWay'),
     (6.99, 'grilled cheese', 'GrilledCheeseHouse'),
-    (5.99, 'baby food', 'BabyFoodHouse'),
-    (6.99, 'baby oatmeal', 'BabyFoodHouse');
+    (5.99, 'kebab', 'KebabQuai'),
+    (6.99, 'shawarma', 'KebabQuai');
 
 insert into account values
-    (100.00, 'peter.griffin@gmail.com'),
-    (200.00, 'joe.swanson@gmail.com'),
-    (150.00, 'glenn.quagmire@gmail.com'),
-    (75.00, 'adam.west@gmail.com'),
-    (125.00, 'diane.simmons@gmail.com'),
-    (50.00, 'tom.tucker@gmail.com');
+    (100.00, 'peter.Smith@gmail.com'),
+    (200.00, 'joe.Taylor@gmail.com'),
+    (150.00, 'glenn.Li@gmail.com'),
+    (75.00, 'adam.Johnson@gmail.com'),
+    (125.00, 'diane.Wilson@gmail.com'),
+    (50.00, 'tom.Chase@gmail.com');
 
 insert into employee values
-    ('Gordon.Ramsay@gmail.com', 1, 'Gordon', 'Ramsay', 'MaceroniHouse'),
-    ('Jamie.Oliver@gmail.com', 2, 'Jamie', 'Oliver', 'CornHouse'),
+    ('Gordon.Ramsay@gmail.com', 1, 'Gordon', 'Ramsay', 'PastaPlace'),
+    ('Jamie.Oliver@gmail.com', 2, 'Jamie', 'Oliver', 'NoodleWay'),
     ('Nigella.Lawson@gmail.com', 3, 'Nigella', 'Lawson', 'GrilledCheeseHouse'),
-    ('Bobby.Flay@gmail.com', 4, 'Bobby', 'Flay', 'BabyFoodHouse'),
-    ('Emeril.Lagasse@gmail.com', 5, 'Emeril', 'Lagasse', 'MaceroniHouse'),
-    ('Wolfgang.Puck@gmail.com', 6, 'Wolfgang', 'Puck', 'CornHouse'),
+    ('Bobby.Flay@gmail.com', 4, 'Bobby', 'Flay', 'KebabQuai'),
+    ('Emeril.Lagasse@gmail.com', 5, 'Emeril', 'Lagasse', 'PastaPlace'),
+    ('Wolfgang.Puck@gmail.com', 6, 'Wolfgang', 'Puck', 'NoodleWay'),
     ('Tom.Colicchio@gmail.com', 7, 'Tom', 'Colicchio', 'GrilledCheeseHouse'),
-    ('Mario.Batali@gmail.com', 8, 'Mario', 'Batali', 'BabyFoodHouse'),
-    ('Rachael.Ray@gmail.com', 9, 'Rachael', 'Ray', 'MaceroniHouse'),
-    ('Guy.Fieri@gmail.com', 10, 'Guy', 'Fieri', 'CornHouse'),
+    ('Mario.Batali@gmail.com', 8, 'Mario', 'Batali', 'KebabQuai'),
+    ('Rachael.Ray@gmail.com', 9, 'Rachael', 'Ray', 'PastaPlace'),
+    ('Guy.Fieri@gmail.com', 10, 'Guy', 'Fieri', 'NoodleWay'),
     ('Robert.Irvine@gmail.com', 11, 'Robert', 'Irvine', 'GrilledCheeseHouse'),
-    ('Anne.Burrell@gmail.com', 12, 'Anne', 'Burrell', 'BabyFoodHouse'),
-    ('Padma.Lakshmi@gmail.com', 13, 'Padma', 'Lakshmi', 'MaceroniHouse'),
-    ('Tyrone.Gilliams@gmail.com', 14, 'Tyrone', 'Gilliams', 'CornHouse'),
+    ('Anne.Burrell@gmail.com', 12, 'Anne', 'Burrell', 'KebabQuai'),
+    ('Padma.Lakshmi@gmail.com', 13, 'Padma', 'Lakshmi', 'PastaPlace'),
+    ('Tyrone.Gilliams@gmail.com', 14, 'Tyrone', 'Gilliams', 'NoodleWay'),
     ('Duff.Goldman@gmail.com', 15, 'Duff', 'Goldman', 'GrilledCheeseHouse'),
-    ('Alex.Guarnaschelli@gmail.com', 16, 'Alex', 'Guarnaschelli', 'BabyFoodHouse'),
-    ('Buddy.Valastro@gmail.com', 17, 'Buddy', 'Valastro', 'MaceroniHouse'),
-    ('Katie.Lee@gmail.com', 18, 'Katie', 'Lee', 'CornHouse'),
+    ('Alex.Guarnaschelli@gmail.com', 16, 'Alex', 'Guarnaschelli', 'KebabQuai'),
+    ('Buddy.Valastro@gmail.com', 17, 'Buddy', 'Valastro', 'PastaPlace'),
+    ('Katie.Lee@gmail.com', 18, 'Katie', 'Lee', 'NoodleWay'),
     ('Sandra.Lee@gmail.com', 19, 'Sandra', 'Lee', 'GrilledCheeseHouse'),
-    ('Aarón.Sánchez@gmail.com', 20, 'Aarón', 'Sánchez', 'BabyFoodHouse'),
-    ('Andrew.Zimmern@gmail.com', 21, 'Andrew', 'Zimmern', 'MaceroniHouse'),
-    ('Alton.Brown@gmail.com', 22, 'Alton', 'Brown', 'CornHouse'),
+    ('Aarón.Sánchez@gmail.com', 20, 'Aarón', 'Sánchez', 'KebabQuai'),
+    ('Andrew.Zimmern@gmail.com', 21, 'Andrew', 'Zimmern', 'PastaPlace'),
+    ('Alton.Brown@gmail.com', 22, 'Alton', 'Brown', 'NoodleWay'),
     ('Michael.Symon@gmail.com', 23, 'Michael', 'Symon', 'GrilledCheeseHouse'),
-    ('Bristol.Palin@gmail.com', 24, 'Bristol', 'Palin', 'BabyFoodHouse');
+    ('Bristol.Palin@gmail.com', 24, 'Bristol', 'Palin', 'KebabQuai');
 
 insert into shift values
     ('2023-02-11', '08:00:00', '12:00:00', 1),
@@ -211,21 +211,21 @@ insert into manager values
     (19), (20), (21), (22), (23), (24);
 
 insert into customerRelation values
-    ('peter.griffin@gmail.com', 1),
-    ('joe.swanson@gmail.com', 2),
-    ('glenn.quagmire@gmail.com', 3),
-    ('adam.west@gmail.com', 4),
-    ('diane.simmons@gmail.com', 5),
-    ('tom.tucker@gmail.com', 6);
+    ('Peter.Smith@gmail.com', 1),
+    ('Joe.Taylor@gmail.com', 2),
+    ('Glenn.Li@gmail.com', 3),
+    ('Adam.Johnson@gmail.com', 4),
+    ('Diane.Wilson@gmail.com', 5),
+    ('Tom.Chase@gmail.com', 6);
 
 insert into rorder values
-    ('peter.griffin@gmail.com', 7, 'MaceroniHouse', 1, 10.99, 1.00, '12:00:00', '11:00:00', '2022-12-01'),
-    ('joe.swanson@gmail.com', 8, 'CornHouse', 2, 7.99, 0.50, '13:00:00', '12:00:00', '2022-12-02'),
-    ('glenn.quagmire@gmail.com', 9, 'GrilledCheeseHouse',  3, 6.99, 0.75, '14:00:00', '13:00:00', '2022-12-03'),
-    ('adam.west@gmail.com', 10, 'CornHouse',  4, 9.99, 0.25, '15:00:00', '14:00:00', '2022-12-04'),
-    ('diane.simmons@gmail.com', 11, 'MaceroniHouse',  5, 10.99, 0.50, '16:00:00', '15:00:00', '2022-12-05'),
-    ('tom.tucker@gmail.com', 12, 'BabyFoodHouse',  6, 5.99, 0.10, '17:00:00', '16:00:00', '2022-12-05'),
-    ('tom.tucker@gmail.com', 11, 'BabyFoodHouse',  7, 4.99, 0.13, '19:00:00', '20:00:00', '2022-12-05');
+    ('Peter.Smith@gmail.com', 7, 'PastaPlace', 1, 10.99, 1.00, '12:00:00', '11:00:00', '2022-12-01'),
+    ('Joe.Taylor@gmail.com', 8, 'NoodleWay', 2, 7.99, 0.50, '13:00:00', '12:00:00', '2022-12-02'),
+    ('Glenn.Li@gmail.com', 9, 'GrilledCheeseHouse',  3, 6.99, 0.75, '14:00:00', '13:00:00', '2022-12-03'),
+    ('Adam.Johnson@gmail.com', 10, 'NoodleWay',  4, 9.99, 0.25, '15:00:00', '14:00:00', '2022-12-04'),
+    ('Diane.Wilson@gmail.com', 11, 'PastaPlace',  5, 10.99, 0.50, '16:00:00', '15:00:00', '2022-12-05'),
+    ('Tom.Chase@gmail.com', 12, 'KebabQuai',  6, 5.99, 0.10, '17:00:00', '16:00:00', '2022-12-05'),
+    ('Tom.Chase@gmail.com', 11, 'KebabQuai',  7, 4.99, 0.13, '19:00:00', '20:00:00', '2022-12-05');
 
 insert into certification values
     ('deep frier training', 1),
@@ -233,23 +233,23 @@ insert into certification values
     ('pastry master', 3),
     ('bakery level 15 certified', 4),
     ('wine specialist', 5),
-    ('babyfood master', 6);
+    ('oven master', 6);
 
 insert into payment values
-    (10.99, '2022-12-01', 'Peter.Griffin@gmail.com'),
-    (9.99, '2022-12-02', 'Peter.Griffin@gmail.com'),
-    (8.99, '2022-12-03', 'Peter.Griffin@gmail.com'),
-    (7.99, '2022-12-04', 'Peter.Griffin@gmail.com'),
-    (6.99, '2022-12-05', 'Peter.Griffin@gmail.com'),
-    (5.99, '2022-12-06', 'Peter.Griffin@gmail.com');
+    (10.99, '2022-12-01', 'Peter.Smith@gmail.com'),
+    (9.99, '2022-12-02', 'Peter.Smith@gmail.com'),
+    (8.99, '2022-12-03', 'Peter.Smith@gmail.com'),
+    (7.99, '2022-12-04', 'Peter.Smith@gmail.com'),
+    (6.99, '2022-12-05', 'Peter.Smith@gmail.com'),
+    (5.99, '2022-12-06', 'Peter.Smith@gmail.com');
 
 insert into itemsOrdered (resname, iname, orid, email, id) values
-    ('MaceroniHouse', 'maceroni', 1, 'peter.griffin@gmail.com', 7),
-    ('CornHouse', 'hot corn', 2, 'joe.swanson@gmail.com', 8),
-    ('GrilledCheeseHouse', 'grilled cheese', 3, 'glenn.quagmire@gmail.com', 9),
-    ('CornHouse', 'warm corn', 4, 'adam.west@gmail.com', 10),
-    ('MaceroniHouse', 'maceroni', 5, 'diane.simmons@gmail.com', 11),
-    ('BabyFoodHouse', 'baby food', 6, 'tom.tucker@gmail.com', 12),
-    ('BabyFoodHouse', 'baby oatmeal', 6, 'tom.tucker@gmail.com', 12),
-    ('BabyFoodHouse', 'baby oatmeal', 7, 'tom.tucker@gmail.com', 11);
+    ('PastaPlace', 'pasta', 1, 'Peter.Smith@gmail.com', 7),
+    ('NoodleWay', 'vegan noodles', 2, 'Joe.Taylor@gmail.com', 8),
+    ('GrilledCheeseHouse', 'grilled cheese', 3, 'Glenn.Li@gmail.com', 9),
+    ('NoodleWay', 'warm noodles', 4, 'Adam.Johnson@gmail.com', 10),
+    ('PastaPlace', 'pasta', 5, 'Diane.Wilson@gmail.com', 11),
+    ('KebabQuai', 'kebab', 6, 'Tom.Chase@gmail.com', 12),
+    ('KebabQuai', 'shawarma', 6, 'Tom.Chase@gmail.com', 12),
+    ('KebabQuai', 'shawarma', 7, 'Tom.Chase@gmail.com', 11);
 
