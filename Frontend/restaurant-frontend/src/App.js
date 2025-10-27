b4f108f2a9a1d860be5d4cbba6db84675111d13a
19ndc2 <19ndc2@queensu.ca>
2025-10-27 19:02:38 -0400

    added package.json and app.js file changes

--- Frontend/restaurant-frontend/src/App.js
+++ Frontend/restaurant-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import NewCustomer from './pages/NewCustomer.jsx';
 import Orders from './pages/Orders.jsx';
